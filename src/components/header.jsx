3dd5291c58ac4361d00786ee39c5d9a710488a85
Sajeeb <sajeebahamed6@gmail.com>
2021-02-04 15:08:57 +0600

    header

---
+++ src/components/header.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { newsCategory } from '../news/index';
+class Header extends Component {
+    state = {
+        searchTerm: ''
+    }
+    handleChange = e => {
+        this.setState({
+            searchTerm: e.target.value
+        })
+    }
+    handleKeyPress = e => {
+        // TODO: Implement Later
+    }
+    render() {
+        const { category } = this.props
+        return (
+            <div className='my-4'>
+                <h1 className='mb-4' style={{fontWeight: '300'}}>
+                    Top News Headlines
+                </h1>
+                <input
+                    type="search"
+                    className='form-control'
+                    placeholder='Type anything & press enter to search'
+                    value={this.state.searchTerm}
+                    onChange={this.handleChange}
+                    onKeyPress={this.handleKeyPress}
+                />
+                <div className='my-4'>
+                    { newsCategory && 
+                        Object.keys(newsCategory).map((item) => {
+                            if(category === newsCategory[item]) {
+                                return (
+                                    <button className='btn btn-sm btn-warning mr-2 mb-2'>
+                                        { `#${newsCategory[item]}` }
+                                    </button>
+                                )
+                            }
+                            return (
+                                <button className='btn btn-sm btn-light mr-2 mb-2'>
+                                    { `#${newsCategory[item]}` }
+                                </button>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Header;
